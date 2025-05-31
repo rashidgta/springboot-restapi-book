ee41e9f90a5553bd43046b37cf65654cf0a773ac
rashid.gta <rashid.gta@gmail.com>
2025-05-31 14:29:44 +0530

    updated POM.xml and Book.java

--- src/main/java/com/springboot/restapi/book/entities/Book.java
+++ src/main/java/com/springboot/restapi/book/entities/Book.java
@@ -1,14 +1,19 @@
 package com.springboot.restapi.book.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
+@Entity
 @Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "book_id")
     private int id;
     private String title;
     private String author;
